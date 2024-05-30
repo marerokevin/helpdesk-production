f2fce785d02e2b3b6687aafcb48b1578bd3cf7dc
marerokevin <marerokevin@gmail.com>
2024-05-30 13:01:32 +0800

    helpdesk production may 30 2024

---
+++ includes/connect.php
@@ -0,0 +1,15 @@
+<?php
+date_default_timezone_set('Asia/Manila');
+
+
+$servername = "localhost";
+$username = "root";
+$password = "Gpi242$$$";
+$dbname = "helpdesk_db";
+// Create connection
+$con = mysqli_connect($servername, $username, $password, $dbname);
+
+if(!$con = mysqli_connect($servername, $username, $password, $dbname)){
+    die("Failed to Connect to Database!");
+}
+?>
