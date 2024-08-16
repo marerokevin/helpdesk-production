dc66ea2daa19b40592ed5915cbf461fd7d9faee8
cedrickorozo <cedrickorozo@gmail.com>
2024-08-16 14:13:15 +0800

    beta release

--- includes/connect.php
+++ includes/connect.php
@@ -2,13 +2,9 @@
 date_default_timezone_set('Asia/Manila');
 
 
-#$servername = "192.168.5.6";
 $servername = "localhost";
-#$username = "helpdeskts2";
 $username = "root";
-#$password = "gpi242$$$";
 $password = "Gpi242$$$";
-#$dbname = "helpdesk_db_test";
 $dbname = "helpdesk_db";
 // Create connection
 $con = mysqli_connect($servername, $username, $password, $dbname);
