5ae2cc09297246312a22581df378e45c2cdcb3d8
cedrickjames <cedrickorozo@gmail.com>
2024-10-16 10:05:37 +0800

    change connect credentials

--- includes/connect.php
+++ includes/connect.php
@@ -2,10 +2,10 @@
 date_default_timezone_set('Asia/Manila');
 
 
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "helpdesk_db";
+$servername = "192.168.5.6";
+$username = "helpdeskts3";
+$password = "gpi242$$$";
+$dbname = "helpdesk_db_test";
 // Create connection
 $con = mysqli_connect($servername, $username, $password, $dbname);
 
