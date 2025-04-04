d19c90c086c99693b6eb8c66646759d050c96b7b
cedrickjames <cedrickorozo@gmail.com>
2025-04-04 09:22:19 +0800

    Merge branch 'helpdesk-v3' of https://github.com/cedrickjames/helpdesk-production into helpdesk-v3

--- includes/connect.php
+++ includes/connect.php
@@ -2,10 +2,12 @@
 date_default_timezone_set('Asia/Manila');
 
 
+
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "helpdesk_db";
+
 // Create connection
 $con = mysqli_connect($servername, $username, $password, $dbname);
 
